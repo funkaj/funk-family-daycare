2fa50a45c5241256493e0dc273901a335de3c47b
Aramoro <35650608+Yogo-Isoroku@users.noreply.github.com>
2019-10-17 23:58:44 -0700

    added art assets to navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,6 +4,10 @@ import github from '../img/github-icon.svg';
 import facebook from '../img/social/facebook.svg';
 import instagram from '../img/social/instagram.svg';
 import piggy from '../img/piggy.svg';
+import dino from '../img/dino.svg';
+import ducky from '../img/ducky.svg';
+import robo from '../img/robo.svg';
+
 const Navbar = class extends React.Component {
 	constructor(props) {
 		super(props);
@@ -43,7 +47,7 @@ const Navbar = class extends React.Component {
 					<div className='navbar-brand'>
 						<Link to='/' className='navbar-item' title='Home'>
 							{/* Create logo for FFC */}
-							<img src={piggy} alt="Funk Family Childcare" style={{ width: '88px', minHeight: '100px' }} />
+							
 							<h1 style={{ width: '88px', color: '#ff4400' }}>
 								Funk Family Childcare
 							</h1>
@@ -79,12 +83,34 @@ const Navbar = class extends React.Component {
                 Form Examples
               </Link> */}
 						</div>
+						<div>
+							<img
+								src={piggy}
+								alt='Funk Family Childcare'
+								style={{ width: '88px', minHeight: '100px' }}
+							/>
+							<img
+								src={dino}
+								alt='Dino'
+								style={{ width: '88px', minHeight: '100px' }}
+							/>
+							<img
+								src={robo}
+								alt='Robo'
+								style={{ width: '88px', minHeight: '100px' }}
+							/>
+							<img
+								src={ducky}
+								alt='Ducky'
+								style={{ width: '88px', minHeight: '100px' }}
+							/>
+						</div>
 						<div className='navbar-end has-text-centered'>
 							<a
 								className='navbar-item'
 								title='facebook'
 								href='https://facebook.com'
-                rel='noopener noreferrer'>
+								rel='noopener noreferrer'>
 								<span className='icon'>
 									<img src={facebook} alt='Facebook' />
 								</span>
@@ -93,7 +119,7 @@ const Navbar = class extends React.Component {
 								className='navbar-item'
 								title='instagram'
 								href='https://instagram.com'
-                rel='noopener noreferrer'>
+								rel='noopener noreferrer'>
 								<span className='icon'>
 									<img src={instagram} alt='Instagram' />
 								</span>
